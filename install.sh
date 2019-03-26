2b06eb0f8da9934772bb3724655152a5a75af7b1
Riccardo Salzer <salzamt@live.com>
2019-03-26 08:19:31 +0100

    add dunst config

--- install.sh
+++ install.sh
@@ -76,3 +76,11 @@ sudo apt install docker-ce nordvpn
 
 # pycharm
 umake ide pycharm-professional
+
+
+# install dunst
+apt install libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev
+sudo apt install libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev libnotify-dev
+cd workspace
+git clone https://github.com/dunst-project/dunst.git && cd dunst && nmake && nsudo make install
+
