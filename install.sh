80e372957929270efbf5105a36174dbb0415badc
Riccardo Salzer <salzamt@live.com>
2019-04-05 11:20:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,6 +90,7 @@ sudo apt install gimp
 sudo apt install kolourpaint
 sudo apt install texlive
 
+pip install ranger-fm
 
 # install dunst
 apt install libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev
