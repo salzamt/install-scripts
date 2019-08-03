b7e92a33424a3be80b4e1ab995019725db0068ef
Riccardo Salzer <salzamt@live.com>
2019-08-03 16:55:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -136,3 +136,11 @@ sudo modprobe binder_linux
 #uncomplicated firewall
 ufw enable
 uwf allow in ssh
+
+# polybar
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+cd workspace
+git clone --recursive https://github.com/jaagr/polybar
+cd polybar
+./build.sh
+cp /usr/local/share/doc/polybar/config ~/.config/polybar; polybar -c ~/.config/polybar example
