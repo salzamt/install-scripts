ab1d3d647a13110740c009d541680cf8723641e8
Riccardo Salzer <salzamt@live.com>
2019-08-18 10:17:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -140,6 +140,10 @@ sudo modprobe binder_linux
 ufw enable
 uwf allow in ssh
 
+# install alacritty (gpu accelerated terminal)
+sudo add-apt-repository ppa:mmstick76/alacritty
+sudo apt install alacritty
+
 # polybar
 sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 cd workspace
