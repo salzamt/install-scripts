b3c3ff3ed45662757a8ae9aaa206f05bef5988ce
Riccardo Salzer <salzamt@live.com>
2019-09-05 20:36:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -147,6 +147,10 @@ ln -s ~/.config/tmux/.tmux.conf ~/.tmux.conf
 sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt install alacritty
 
+# disable power button shutdown action - set to ignore.
+echo "HandlePowerKey=suspend" | sudo tee -a /etc/systemd/logind.conf
+sudo service systemd-logind suspend
+
 # polybar
 sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 cd workspace
