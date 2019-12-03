1fb3521d7c0f74c90d5fe69b4b8c3ff427429b9a
Riccardo Salzer <salzamt@live.com>
2019-12-03 14:39:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # change gtk theme to dark
 vim ~/.config/gtk-3.0/settings.ini
 # gtk-theme-name=Adwaita-dark
+# gtk-icon-theme-name=Adwaita
 
 
 sudo apt-get update && sudo apt-get upgrade && apt-get install curl
