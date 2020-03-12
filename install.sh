851c765ccb05039b2d167d54aa72433dad9ce703
Riccardo Salzer <salzamt@live.com>
2020-03-12 17:24:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -189,6 +189,9 @@ sudo apt install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
+
 ###############################################################################
 ######################### pycharm #####@#######################################
 ###############################################################################
