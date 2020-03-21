a3db2ea596bbb3937f907be55daf5deed7630100
Riccardo Salzer <salzamt@live.com>
2020-03-21 10:32:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -319,10 +319,11 @@ systemctl enable i3lock.service
 
 # polybar
 sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
+
 cd workspace
 git clone --recursive https://github.com/jaagr/polybar
-cd polybar
-./build.sh
+cd polybar && ./build.sh
 cp /usr/local/share/doc/polybar/config ~/.config/polybar; polybar -c ~/.config/polybar example
 
 
