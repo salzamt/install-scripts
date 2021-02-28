bed41e3c1a8458baf6c30107f6ace1fcde78ffaa
Riccardo Salzer <salzamt@live.com>
2021-02-28 12:35:29 +0100

    Update install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -4,3 +4,26 @@ $ /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/s
 # apt update
 # apt install i3
 cp config ~/.config/i3/config
+
+
+
+
+Add a file /etc/systemd/system/i3lock.service with the following contents:
+
+# source: https://bbs.archlinux.org/viewtopic.php?pid=1170536#p1170536 by 65kid
+#
+[Unit]
+Description=i3lock
+Before=sleep.target
+
+[Service]
+User=the_login_of_the_user_that_suspends
+Type=forking
+Environment=DISPLAY=:0
+ExecStart=/usr/bin/i3lock
+
+[Install]
+WantedBy=sleep.target
+
+# now enable it with
+sudo systemctl enable i3lock.service
