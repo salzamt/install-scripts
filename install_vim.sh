169ecdc5e3966f66007b92dd7ff02bb0b6097319
Riccardo Salzer <salzamt@live.com>
2021-11-10 23:29:04 +0100

    Create install_vim.sh

---
+++ install_vim.sh
@@ -0,0 +1,6 @@
+cd ~/.vim/bundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-rails.git
+vim -u NONE -c "helptags vim-rails/doc" -c q
+cd ~/.vim/bundle && git clone https://github.com/tpope/vim-sensible.git
