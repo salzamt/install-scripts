17d2318b1d5922d118b859ad2ff82141ca74971b
Riccardo Salzer <116647578+das-salzamt@users.noreply.github.com>
2023-02-25 00:33:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -650,4 +650,15 @@ git config --global core.excludesfile ~/Documents/gitignore
 sudo add-apt-repository ppa:fontforge/fontforge
 sudo apt install fonts-fantasque-sans
 
-
+# op for opswd in zsh 1pass
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+sudo apt update && sudo apt install 1password-cli
