4efd625e4f44b3d0c1a69d5ca3bbd3df6cb68646
Riccardo Salzer <5294942+salzamt@users.noreply.github.com>
2023-03-05 16:31:03 +0100

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -1,5 +1,3 @@
-sudo apt install silversearcher-ag
-
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/tpope/vim-rails.git
