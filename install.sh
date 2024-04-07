5c9d4f7e0cfa7fe77fbd1488830e651115a09fba
Riccardo Salzer <116647578+das-salzamt@users.noreply.github.com>
2024-04-07 14:00:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,6 +72,7 @@ sudo apt install curl wget snapd vim python-pip python3-pip hardinfo \
   silversearcher-ag # for vim fzf
   dialog # bash dialog for pomodoro
   neofetch # for showing terminal os info on startup
+  wavemon # wifi singal monitoring 
 
 # set default editor to vim
 sudo update-alternatives --set editor /usr/bin/vim.basic
