36e7c55557f586094b306c9b89f0b20d4e477f0c
Riccardo Salzer <5294942+salzamt@users.noreply.github.com>
2025-05-12 17:58:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,6 +79,7 @@ sudo apt install curl wget snapd vim python-pip python3-pip hardinfo \
   texlive-bibtex-extra biber # latex bib lib
   obs-studio # recording presentations and streaming
   zathura # pdf viewing for presentation  zathura --mode presentation main.pdf
+  pandoc # PDF generator for vim
 
 # set default editor to vim
 sudo update-alternatives --set editor /usr/bin/vim.basic
