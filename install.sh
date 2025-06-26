b3bcbd2af5ee1bfdb50c487f2ca5b624a1d6cbd2
Riccardo Salzer <5294942+salzamt@users.noreply.github.com>
2025-06-26 14:23:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,6 +69,7 @@ sudo apt install curl wget snapd vim python-pip python3-pip hardinfo \
   apt-transport-https ca-certificates yamllint htop pavucontrol gparted gpart stress kazam flatpack \
   fzf # cli fuzzy finder
   hstr # for zsh history searcj
+  gettext # PO Files
   ncdu # interactive disk utilization utility
   pcmanfm lxappearance
   figlet # FIGlet - display large characters made up of ordinary screen characters
