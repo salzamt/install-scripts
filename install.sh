a9dcc97d75f2bf8055467c3f55de4795ff39c92d
Riccardo Salzer <5294942+salzamt@users.noreply.github.com>
2025-07-14 13:53:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,6 +88,7 @@ sudo apt install curl wget snapd vim python-pip python3-pip hardinfo \
   zathura # pdf viewing for presentation  zathura --mode presentation main.pdf
   pandoc # PDF generator for vim
   heif-convert # concert command for heic files
+  taskwarrior
 
 # set default editor to vim
 sudo update-alternatives --set editor /usr/bin/vim.basic
