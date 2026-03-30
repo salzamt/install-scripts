fdcb03f49c62cd96237040210b8003d3a8e49ad6
Riccardo Salzer <5294942+salzamt@users.noreply.github.com>
2026-03-30 19:05:19 +0200

    Update install.sh
    docker compose

--- install.sh
+++ install.sh
@@ -389,19 +389,20 @@ sudo apt-get install \
     gnupg2 \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+#sudo apt-key fingerprint 0EBFCD88
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # isntall docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
-sudo groupadd docker
-sudo usermod -aG docker $(whoami)
+#TODO: needed?
+#sudo groupadd docker
+#sudo usermod -aG docker $(whoami)
 
 ###############################################################################
 ######################### github cli #############################################
